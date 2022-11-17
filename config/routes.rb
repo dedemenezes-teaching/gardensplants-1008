997fafc96f2704e8a20d222cb7b46e6df30f4781
Andre Menezes <djdedemenezes@gmail.com>
2022-11-17 18:36:51 -0300

    adding tom select

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :gardens do
     resources :plants, only: :create
   end
-  # WE NEED TO KNOW THE ID OF THE PLANT
+  # WE NEED TO KNOW THE ID OF THE PARENT
   resources :plants, only: :destroy do
     resources :plant_tags, only: [:new, :create]
   end
